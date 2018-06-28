5b27619c7f86a4505e8daa8ee85a2cfdc4e48f65
Matt Nadareski <mnadareski@outlook.com>
2018-06-28 10:01:52 -0700

    Add Test project; Migrate so that there's not an external folder

---
+++ DICUI.Test/ConvertersTest.cs
@@ -0,0 +1,69 @@
+﻿using DICUI.Data;
+using DICUI.Utilities;
+using Xunit;
+
+namespace DICUI.Test
+{
+    public class ConvertersTest
+    {
+        [Theory]
+        [InlineData(DICCommands.Audio, MediaType.CD)]
+        [InlineData(DICCommands.BluRay, MediaType.BluRay)]
+        [InlineData(DICCommands.Close, null)]
+        [InlineData(DICCommands.CompactDisc, MediaType.CD)]
+        [InlineData(DICCommands.Data, MediaType.CD)]
+        [InlineData(DICCommands.DigitalVideoDisc, MediaType.DVD)]
+        [InlineData(DICCommands.Eject, null)]
+        [InlineData(DICCommands.Floppy, MediaType.Floppy)]
+        [InlineData(DICCommands.GDROM, MediaType.GDROM)]
+        [InlineData(DICCommands.MDS, null)]
+        [InlineData(DICCommands.Reset, null)]
+        [InlineData(DICCommands.Start, null)]
+        [InlineData(DICCommands.Stop, null)]
+        [InlineData(DICCommands.Sub, null)]
+        [InlineData(DICCommands.Swap, MediaType.GDROM)]
+        [InlineData(DICCommands.XBOX, MediaType.DVD)]
+        public void BaseCommandToMediaTypeTest(string command, MediaType? expected)
+        {
+            MediaType? actual = Converters.BaseCommmandToMediaType(command);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(DICCommands.Audio, KnownSystem.AudioCD)]
+        [InlineData(DICCommands.BluRay, KnownSystem.SonyPlayStation3)]
+        [InlineData(DICCommands.Close, null)]
+        [InlineData(DICCommands.CompactDisc, KnownSystem.IBMPCCompatible)]
+        [InlineData(DICCommands.Data, KnownSystem.IBMPCCompatible)]
+        [InlineData(DICCommands.DigitalVideoDisc, KnownSystem.IBMPCCompatible)]
+        [InlineData(DICCommands.Eject, null)]
+        [InlineData(DICCommands.Floppy, KnownSystem.IBMPCCompatible)]
+        [InlineData(DICCommands.GDROM, KnownSystem.SegaDreamcast)]
+        [InlineData(DICCommands.MDS, null)]
+        [InlineData(DICCommands.Reset, null)]
+        [InlineData(DICCommands.Start, null)]
+        [InlineData(DICCommands.Stop, null)]
+        [InlineData(DICCommands.Sub, null)]
+        [InlineData(DICCommands.Swap, KnownSystem.SegaDreamcast)]
+        [InlineData(DICCommands.XBOX, KnownSystem.MicrosoftXBOX)]
+        public void BaseCommandToKnownSystemTest(string command, KnownSystem? expected)
+        {
+            KnownSystem? actual = Converters.BaseCommandToKnownSystem(command);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(MediaType.CD, ".bin")]
+        [InlineData(MediaType.DVD, ".iso")]
+        [InlineData(MediaType.LaserDisc, ".raw")]
+        [InlineData(MediaType.WiiUOpticalDisc, ".wud")]
+        [InlineData(MediaType.Floppy, ".img")]
+        [InlineData(MediaType.Cassette, ".wav")]
+        [InlineData(MediaType.NONE, null)]
+        public void MediaTypeToExtensionTest(MediaType? mediaType, string expected)
+        {
+            string actual = Converters.MediaTypeToExtension(mediaType);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DICUI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DICUI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DICUI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7cc064d2-38ab-4a05-8519-28660de4562a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
