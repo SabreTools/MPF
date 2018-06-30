0e4f49ef59a974dcd9675b887073c6a83ad69d5b
Matt Nadareski <mnadareski@outlook.com>
2018-06-29 21:41:32 -0700

    Namespace cleanup

--- DICUI/Tasks.cs
+++ DICUI/Tasks.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using DICUI.Data;
