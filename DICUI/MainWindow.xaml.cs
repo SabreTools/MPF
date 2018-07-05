f510a4548d807ca9c7e38d348767c93b1393c7cc
Matt Nadareski <mnadareski@outlook.com>
2018-07-05 16:57:04 -0700

    Namespace cleanups

--- DICUI/MainWindow.xaml.cs
+++ DICUI/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using WinForms = System.Windows.Forms;
