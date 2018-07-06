fbe791f6fdec64adbf4b5917160dee04b75ab708
Matt Nadareski <mnadareski@outlook.com>
2018-07-05 17:30:57 -0700

    Move stuff to DumpEnvironment
    Most of the stuff in Tasks.cs acted on a single input parameter, namely a DumpEnvironment. Since that's the case, it was more logical to wrap those into DumpEnvironment and make them instance methods rather than keep them static. Due to this change, quite a few methods changed access, and some had to be marked internal due to wanting them to be tested separately.

--- DICUI/Properties/AssemblyInfo.cs
+++ DICUI/Properties/AssemblyInfo.cs
@@ -53,3 +53,6 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Anything marked as internal can be used by the test methods
+[assembly: InternalsVisibleTo("DICUI.Test")]
