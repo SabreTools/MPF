f5f779e9786efc0c004ca92b301c6c7a6b1cbf57
Matt Nadareski <mnadareski@outlook.com>
2018-07-18 23:34:41 -0700

    Minor improvements

--- DICUI/Utilities/DumpEnvironment.cs
+++ DICUI/Utilities/DumpEnvironment.cs
@@ -217,12 +217,12 @@ namespace DICUI.Utilities
 
             // Execute additional tools
             progress?.Report(Result.Success("Running any additional tools... please wait!"));
-            result = ExecuteAdditionalToolsAfterDIC();
+            result = await Task.Run(() => ExecuteAdditionalToolsAfterDIC());
             progress?.Report(result);
 
             // Verify dump output and save it
             progress?.Report(Result.Success("Gathering submission information... please wait!"));
-            result = VerifyAndSaveDumpOutput();
+            result = await Task.Run(() => VerifyAndSaveDumpOutput());
             progress?.Report(Result.Success("All submission information gathered!"));
 
             return result;
@@ -294,10 +294,10 @@ namespace DICUI.Utilities
                         return Result.Failure("Error! Could not find subdump!");
 
                     ExecuteSubdump();
-                    break;
+                    return Result.Success("subdump has finished!");
             }
 
-            return Result.Success();
+            return Result.Success("No external tools needed!");
         }
 
         /// <summary>
@@ -410,7 +410,8 @@ namespace DICUI.Utilities
                         case KnownSystem.IBMPCCompatible:
                         case KnownSystem.RainbowDisc:
                             mappings[Template.ISBNField] = Template.OptionalValue;
-                            mappings[Template.CopyProtectionField] = GetCopyProtection() ?? Template.RequiredIfExistsValue;
+                            string protection = GetCopyProtection();
+                            mappings[Template.CopyProtectionField] = (!String.IsNullOrWhiteSpace(protection) ? protection : Template.RequiredIfExistsValue);
                             if (File.Exists(combinedBase + "_subIntention.txt"))
                             {
                                 FileInfo fi = new FileInfo(combinedBase + "_subIntention.txt");
@@ -510,7 +511,8 @@ namespace DICUI.Utilities
                         case KnownSystem.IBMPCCompatible:
                         case KnownSystem.RainbowDisc:
                             mappings[Template.ISBNField] = Template.OptionalValue;
-                            mappings[Template.CopyProtectionField] = GetCopyProtection() ?? Template.RequiredIfExistsValue;
+                            string protection = GetCopyProtection();
+                            mappings[Template.CopyProtectionField] = (!String.IsNullOrWhiteSpace(protection) ? protection : Template.RequiredIfExistsValue);
                             if (File.Exists(combinedBase + "_subIntention.txt"))
                             {
                                 FileInfo fi = new FileInfo(combinedBase + "_subIntention.txt");
