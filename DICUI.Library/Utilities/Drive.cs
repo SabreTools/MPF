ed56c92101e4b04512f1130a212758f312ed41db
Matt Nadareski <mnadareski@outlook.com>
2020-10-08 21:21:11 -0700

    Clean up sector read method a bit more (nw)

--- DICUI.Library/Utilities/Drive.cs
+++ DICUI.Library/Utilities/Drive.cs
@@ -63,31 +63,34 @@ namespace DICUI.Utilities
         }
 
         /// <summary>
-        /// Read a sector from the given drive
+        /// Read a sector with a specified size from the drive
         /// </summary>
-        /// <param name="sector">Sector number</param>
+        /// <param name="num">Sector number, non-negative</param>
+        /// <param name="size">Size of a sector in bytes</param>
         /// <returns>Byte array representing the sector, null on error</returns>
-        public byte[] ReadSector(long sector)
+        public byte[] ReadSector(long num, int size = 2048)
         {
             // Missing drive leter is not supported
             if (string.IsNullOrEmpty(this.DriveInfo?.Name))
                 return null;
 
             // We don't support negative sectors
-            if (sector < 0)
+            if (num < 0)
                 return null;
 
-            // Get the starting point
-            int sectorSize = 2048;
-            long start = sector * sectorSize; 
-
-            // Try to read the sector from the device
+            // Wrap the following in case of device access errors
             try
             {
+                // Open the drive as a device
                 var fs = File.OpenRead($"\\\\?\\{this.Letter}:");
+
+                // Seek to the start of the sector, if possible
+                long start = num * size;
                 fs.Seek(start, SeekOrigin.Begin);
-                byte[] buffer = new byte[sectorSize];
-                fs.Read(buffer, 0, sectorSize);
+
+                // Read and return the sector
+                byte[] buffer = new byte[size];
+                fs.Read(buffer, 0, size);
                 return buffer;
             }
             catch
