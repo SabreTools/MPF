26559cfcdeb9992513bce31306581a8165784982
Terry Janas <tjanas@gmail.com>
2022-11-22 14:40:44 -0600

    Populate internal serial and version from PS3 disc

--- MPF.Modules/DD/Parameters.cs
+++ MPF.Modules/DD/Parameters.cs
@@ -126,6 +126,11 @@ namespace MPF.Modules.DD
                     info.VersionAndEditions.Version = GetPlayStation2Version(drive?.Letter) ?? "";
                     break;
 
+                case RedumpSystem.SonyPlayStation3:
+                    info.VersionAndEditions.Version = GetPlayStation3Version(drive?.Letter) ?? "";
+                    info.CommonDiscInfo.CommentsSpecialFields[SiteCode.InternalSerialName] = GetPlayStation3Serial(drive?.Letter) ?? "";
+                    break;
+
                 case RedumpSystem.SonyPlayStation4:
                     info.VersionAndEditions.Version = GetPlayStation4Version(drive?.Letter) ?? "";
                     info.CommonDiscInfo.CommentsSpecialFields[SiteCode.InternalSerialName] = GetPlayStation4Serial(drive?.Letter) ?? "";
