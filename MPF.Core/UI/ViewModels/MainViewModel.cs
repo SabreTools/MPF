af9b0dc214804196a7d3a01b11ea35443f70f58c
Deterous <138427222+Deterous@users.noreply.github.com>
2023-10-11 09:15:47 +1300

    Fix media combobox not updating (#568)
    * Fix media combobox not updating
    
    * Don't call SetCurrentDiscType rather than clear the variable

--- MPF.Core/UI/ViewModels/MainViewModel.cs
+++ MPF.Core/UI/ViewModels/MainViewModel.cs
@@ -1326,9 +1326,6 @@ namespace MPF.Core.UI.ViewModels
             Result result = Tools.GetSupportStatus(_environment.System, _environment.Type);
             this.Status = result.Message;
 
-            // Set the index for the current disc type
-            SetCurrentDiscType();
-
             // Enable or disable the button
             this.StartStopButtonEnabled = result && ShouldEnableDumpingButton();
 
