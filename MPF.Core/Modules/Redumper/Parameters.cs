72341e25ba5c662abd693f35cbd3a26fa4b9864c
Deterous <138427222+Deterous@users.noreply.github.com>
2024-01-16 00:01:33 +1300

    Remove unnecessary using

--- MPF.Core/Modules/Redumper/Parameters.cs
+++ MPF.Core/Modules/Redumper/Parameters.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Text.RegularExpressions;
 using MPF.Core.Converters;
 using MPF.Core.Data;
