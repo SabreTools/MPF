a9ee6667d0ebd60a83532b2ef672783a8589442e
Matt Nadareski <mnadareski@outlook.com>
2024-05-07 19:17:48 -0400

    Add _PFI.bin support for UIC (fixes #696)

--- CHANGELIST.md
+++ CHANGELIST.md
@@ -3,6 +3,7 @@
 - Option for default Redumper leadin retries (Deterous)
 - Omit false positives on formatting protections
 - Critical update to BinaryObjectScanner 3.1.10
+- Add _PFI.bin support for UIC
 
 ### 3.1.7 (2024-04-28)
 
--- MPF.Core/Modules/UmdImageCreator/Parameters.cs
+++ MPF.Core/Modules/UmdImageCreator/Parameters.cs
@@ -114,16 +114,18 @@ namespace MPF.Core.Modules.UmdImageCreator
             {
                 info.Artifacts ??= [];
 
-                if (File.Exists(basePath + "_disc.txt"))
-                    info.Artifacts["disc"] = GetBase64(GetFullFile(basePath + "_disc.txt")) ?? string.Empty;
-                if (File.Exists(basePath + "_drive.txt"))
-                    info.Artifacts["drive"] = GetBase64(GetFullFile(basePath + "_drive.txt")) ?? string.Empty;
-                if (File.Exists(basePath + "_mainError.txt"))
-                    info.Artifacts["mainError"] = GetBase64(GetFullFile(basePath + "_mainError.txt")) ?? string.Empty;
-                if (File.Exists(basePath + "_mainInfo.txt"))
-                    info.Artifacts["mainInfo"] = GetBase64(GetFullFile(basePath + "_mainInfo.txt")) ?? string.Empty;
-                if (File.Exists(basePath + "_volDesc.txt"))
-                    info.Artifacts["volDesc"] = GetBase64(GetFullFile(basePath + "_volDesc.txt")) ?? string.Empty;
+                if (File.Exists($"{basePath}_disc.txt"))
+                    info.Artifacts["disc"] = GetBase64(GetFullFile($"{basePath}_disc.txt")) ?? string.Empty;
+                if (File.Exists($"{basePath}_drive.txt"))
+                    info.Artifacts["drive"] = GetBase64(GetFullFile($"{basePath}_drive.txt")) ?? string.Empty;
+                if (File.Exists($"{basePath}_mainError.txt"))
+                    info.Artifacts["mainError"] = GetBase64(GetFullFile($"{basePath}_mainError.txt")) ?? string.Empty;
+                if (File.Exists($"{basePath}_mainInfo.txt"))
+                    info.Artifacts["mainInfo"] = GetBase64(GetFullFile($"{basePath}_mainInfo.txt")) ?? string.Empty;
+                //if (File.Exists($"{basePath}_PFI.bin"))
+                //    info.Artifacts["pfi"] = Convert.ToBase64String(File.ReadAllBytes($"{basePath}_PFI.bin")) ?? string.Empty;
+                if (File.Exists($"{basePath}_volDesc.txt"))
+                    info.Artifacts["volDesc"] = GetBase64(GetFullFile($"{basePath}_volDesc.txt")) ?? string.Empty;
             }
         }
 
@@ -144,6 +146,9 @@ namespace MPF.Core.Modules.UmdImageCreator
                         logFiles.Add($"{basePath}_mainInfo.txt");
                     if (File.Exists($"{basePath}_volDesc.txt"))
                         logFiles.Add($"{basePath}_volDesc.txt");
+
+                    if (File.Exists($"{basePath}_PFI.bin"))
+                        logFiles.Add($"{basePath}_PFI.bin");
 
                     break;
             }
