eaa66ce07026e4936cf5e0cbf430117492e891d3
Deterous <138427222+Deterous@users.noreply.github.com>
2025-10-18 19:10:43 +0900

    test remove System.Windows.Forms

--- MPF.UI/Windows/OptionsWindow.xaml.cs
+++ MPF.UI/Windows/OptionsWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.Threading;
 #endif
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Forms;
 using MPF.Frontend;
 using MPF.Frontend.ViewModels;
 using SabreTools.RedumpLib.Web;
