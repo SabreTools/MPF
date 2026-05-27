38f10ed385eeee6ee109b05de1ac2358fff24605
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2026-05-27 14:02:35 +0200

    feat(avalonia): scaffold cross-platform MPF.UI.Avalonia project

---
+++ MPF.UI.Avalonia/App.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using MPF.UI.Avalonia.Views;
+
+namespace MPF.UI.Avalonia
+{
+    public partial class App : Application
+    {
+        public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                desktop.MainWindow = new MainWindow();
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ MPF.UI.Avalonia/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using Avalonia;
+
+namespace MPF.UI.Avalonia
+{
+    internal sealed class Program
+    {
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont()
+                .LogToTrace();
+    }
+}
